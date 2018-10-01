edf2b66a93c7855efb80aaea244008bf086bfea9
Mariann M <mariann19@gmail.com>
2018-10-01 11:31:53 -0400

    ready to test

--- wrap.js
+++ wrap.js
@@ -1,2 +1,18 @@
-const wrap = (string, col) => "";
+const wrap = (string, col) =>{
+  if (string.length <= col){
+    return string;
+  }
+  const indexOfBlank = string.lastIndexOf(' ', col)
+  let split;
+  let offset;
+  if (indexOfBlank > -1){
+    split = indexOfBlank;
+    offset =1;
+  } else {
+    split = col;
+    offset =0
+  }
+  return string.substring(0, split) + '\n' + wrap(string.substring(split + offset), col)
+}
 module.exports = wrap;
+
